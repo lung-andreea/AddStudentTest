d7f8f6c55be00780d50d5e869246ccc9fb5ca931
lung-andreea <lung.andreea.cristina@gmail.com>
2019-04-15 00:49:04 +0300

    assignment all paths

--- src/main/java/validation/TemaValidator.java
+++ src/main/java/validation/TemaValidator.java
@@ -9,11 +9,23 @@ public class TemaValidator implements Validator<Tema> {
         if (tema.getDescriere() == null || tema.getDescriere().equals("")) {
             throw new ValidationException("Descriere invalida! \n");
         }
-        if (tema.getDeadline() < 1 || tema.getDeadline() > 14 || tema.getDeadline() < tema.getStartline()) {
+
+        // <--- Code changed from: --->
+//        if (tema.getDeadline() < 1 || tema.getDeadline() > 14 || tema.getDeadline() < tema.getStartline()) {
+//            throw new ValidationException("Deadline invalid! \n");
+//        }
+//        if (tema.getStartline() < 1 || tema.getStartline() > 14 || tema.getStartline() > tema.getDeadline()) {
+//            throw new ValidationException("Data de primire invalida! \n");
+//        }
+        // <--- to: --->
+        if (tema.getDeadline() < 1 || tema.getDeadline() > 14) {
             throw new ValidationException("Deadline invalid! \n");
         }
-        if (tema.getStartline() < 1 || tema.getStartline() > 14 || tema.getStartline() > tema.getDeadline()) {
-            throw new ValidationException("Data de primire invalida! \n");
+        if (tema.getStartline() < 1 || tema.getStartline() > 14) {
+            throw new ValidationException("Startline invalid! \n");
+        }
+        if (tema.getDeadline() < tema.getStartline()) {
+            throw new ValidationException("Startline must be before deadline! \n");
         }
     }
 }
--- src/test/java/domain/AssignmentTest.java
+++ src/test/java/domain/AssignmentTest.java
@@ -35,4 +35,68 @@ public class AssignmentTest {
         }
         assert nr==0;
     }
+
+    @Test
+    public void shouldNotAddAssignmentWithNullID() {
+        Tema assignmentWithNullID = new Tema(null,"Lab03/SSVV",8,5);
+        Tema st1 = assignmentRepo.save(assignmentWithNullID);
+
+        assert st1 == null;
+    }
+
+    @Test
+    public void shouldNotAddAssignmentWithEmptyStringID() {
+        Tema assignmentWithEmptyID = new Tema("","Lab03/SSVV",8,5);
+        Tema st1 = assignmentRepo.save(assignmentWithEmptyID);
+
+        assert st1 == null;
+    }
+
+    @Test
+    public void shouldNotAddAssignmentWithNullDescription() {
+        Tema assignmentWithNullDescription = new Tema("1",null,8,5);
+        Tema st1 = assignmentRepo.save(assignmentWithNullDescription);
+
+        assert st1 == null;
+    }
+
+    @Test
+    public void shouldNotAddAssignmentWithEmptyStringDescription() {
+        Tema assignmentWithEmptyID = new Tema("1","",8,5);
+        Tema st1 = assignmentRepo.save(assignmentWithEmptyID);
+
+        assert st1 == null;
+    }
+
+    @Test
+    public void shouldNotAddAssigmentWithDateOutOfRange() {
+        // Date range is considered the usual duration of a school semester, that is 14 weeks,
+        // so assignment has to have startline and deadline within 1-14.
+
+        Tema assignmentWithInvalidStartline = new Tema("1","Lab03/SSVV",5,0);
+        Tema st1 = assignmentRepo.save(assignmentWithInvalidStartline);
+
+        assert st1 == null;
+
+        Tema assignmentWithInvalidDeadline = new Tema("2","Lab03/SSVV",15,4);
+        Tema st2 = assignmentRepo.save(assignmentWithInvalidDeadline);
+
+        assert st2 == null;
+    }
+
+    @Test
+    public void shouldNotAddTemaIfAlreadyPresent() {
+        Tema assignment1 = new Tema("1","Lab03/SSVV",5,3);
+        assignmentRepo.save(assignment1);
+        Tema assignment2 = new Tema("1","Lab03-02/SSVV",5,3);
+        assignmentRepo.save(assignment2);
+        Iterable values = assignmentRepo.findAll();
+        Iterator it = values.iterator();
+        int nr=0;
+        while (it.hasNext()) {
+            it.next();
+            nr++;
+        }
+        assert nr==1;
+    }
 }
